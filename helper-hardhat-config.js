88f44a65690a97804af1241aabd1688eb61794a0
Jack Willis <jackwillis517@gmail.com>
2022-06-17 17:32:26 -0400

    Finished Raffle.sol and added deploy scripts

---
+++ helper-hardhat-config.js
@@ -0,0 +1,13 @@
+const networkConfig = {
+    4: {
+        names: "rinkeby",
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
